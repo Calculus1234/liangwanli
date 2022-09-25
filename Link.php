f1dba24e416570a1d1f294a51db8884978adace8
Calculus1234 <113374050+Calculus1234@users.noreply.github.com>
2022-09-25 15:25:00 +0800

    Update Link.php

--- Link.php
+++ Link.php
@@ -1,7 +1,7 @@
 header("Content-Type:wordlist/html;charset=utf-8"); //防止中文乱码
 
-$link=mysqli_connect("localhost","wordlist","wordlist"); //服务器地址
+$link=mysqli_connect("wordlist.sql"); //服务器地址
 
-mysqli_select_db($links,"wordlist"); //连接数据库
+mysqli_select_db($links,"wordlist.sql"); //连接数据库
 
 ?>
